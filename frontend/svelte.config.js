78e5643bdce97767560957e0c1beec9e661631ae
drtucfvygbhj <callum.nussbaumer.adrien@gmail.com>
2025-09-30 14:21:13 +0200

    commit

--- frontend/svelte.config.js
+++
@@ -1,9 +0,0 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from '@sveltejs/adapter-auto';
-
-export default {
-  preprocess: vitePreprocess(),
-  kit: {
-    adapter: adapter()
-  }
-};
